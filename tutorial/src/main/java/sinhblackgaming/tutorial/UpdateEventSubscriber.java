56e60b23d751dffc7416d036b8468a61bb401e39
sinhlt58 <sinhlt58@gmail.com>
2020-03-28 22:39:00 +0700

    firt silverfish

--- tutorial/src/main/java/sinhblackgaming/tutorial/UpdateEventSubscriber.java
+++ tutorial/src/main/java/sinhblackgaming/tutorial/UpdateEventSubscriber.java
@@ -1,7 +1,16 @@
 package sinhblackgaming.tutorial;
 
+import net.minecraft.block.Block;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.monster.SilverfishEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
@@ -11,13 +20,44 @@ import org.apache.logging.log4j.Logger;
 public class UpdateEventSubscriber {
 
     public static final Logger LOGGER = LogManager.getLogger();
+    public static int silverFishSpawnedCount = 0;
 
     @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-        if (event.player.world.isRemote){
-            LOGGER.info("onPlayerTick IS REMOTE");
-        } else {
-            LOGGER.info("onPlayerTick NOT REMOTE");
+        PlayerEntity player = event.player;
+        if (!player.world.isRemote) {
+//            BlockPos pos = player.getPosition();
+//            Direction facingDirection = player.getHorizontalFacing();
+//            pos = pos.offset(facingDirection).offset(facingDirection);
+//            pos = pos.up();
+//
+////            SilverfishEntity silverFish = EntityType.SILVERFISH.create(player.world);
+////            silverFish.setPosition(pos.getX(), pos.getY(), pos.getZ());
+////
+////            player.world.addEntity(silverFish);
+//
+//            LOGGER.info("Player pos: " + player.getPosition());
+//            LOGGER.info("Facing direction: " + facingDirection);
+//            LOGGER.info("Offset pos: " + pos);
+        }
+//        if (event.player.world.isRemote){
+//            LOGGER.info("onPlayerTick IS REMOTE");
+//        } else {
+//            LOGGER.info("onPlayerTick NOT REMOTE");
+//        }
+    }
+
+    @SubscribeEvent
+    public static void onBlockBreak(BlockEvent.BreakEvent event) {
+        if (!event.getPlayer().world.isRemote) {
+            PlayerEntity player = event.getPlayer();
+            BlockPos pos = event.getPos();
+
+            SilverfishEntity silverFish = EntityType.SILVERFISH.create(player.world);
+            silverFish.setPosition(pos.getX(), pos.getY(), pos.getZ());
+            player.world.addEntity(silverFish);
+
+//            silverFishSpawnedCount += 1;
         }
     }
 
