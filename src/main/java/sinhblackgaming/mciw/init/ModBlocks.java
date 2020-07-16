baacdbfbb7f70e0737152a5510757b6de7222016
sinhlt58 <sinhlt58@gmail.com>
2020-07-17 00:35:21 +0700

    add meme dog example block

--- src/main/java/sinhblackgaming/mciw/init/ModBlocks.java
+++ src/main/java/sinhblackgaming/mciw/init/ModBlocks.java
@@ -14,7 +14,7 @@ public class ModBlocks {
 
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, MCIWMod.MODID);
 
-    public static final RegistryObject<Block> EXAMPLE_ORE = BLOCKS.register("example_ore", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F, 3.0F)));
+    public static final RegistryObject<Block> EXAMPLE_ORE = BLOCKS.register("example_block", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F, 3.0F)));
 
     public static final RegistryObject<Block> NETHER_BED = BLOCKS.register("nether_bed",
             () -> new NetherBed(DyeColor.RED, Block.Properties.create(Material.WOOL).sound(SoundType.WOOD).hardnessAndResistance(0.2F)));
